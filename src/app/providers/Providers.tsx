eb53fd5bf6fc8f4e39003b6ae2066afce635403f
NamDuong1212 <nguyennamduong1212@gmail.com>
2025-01-07 09:20:41 +0700

    update react hot toast provider

--- src/app/providers/Providers.tsx
+++ src/app/providers/Providers.tsx
@@ -3,27 +3,27 @@
 import { ReactNode } from "react";
 import QueryProvider from "./QueryProvider";
 import AntProvider from "./AntProvider";
-import { Toaster } from "react-hot-toast";
+import { Toaster, ToastOptions } from "react-hot-toast";
 
 interface ProvidersProps {
   children: ReactNode;
 }
+
+const toastOptions: ToastOptions = {
+  duration: 3000,
+  position: "top-center",
+  style: {
+    background: "white",
+    color: "black",
+  },
+};
 
 export default function Providers({ children }: ProvidersProps) {
   return (
     <QueryProvider>
       <AntProvider>
         {children}
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 1000,
-            style: {
-              background: "white",
-              color: "black",
-            },
-          }}
-        />
+        <Toaster toastOptions={toastOptions} />
       </AntProvider>
     </QueryProvider>
   );
